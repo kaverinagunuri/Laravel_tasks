3cbf07e26585dfb9d4a7b44327c6433a19f904f3
Kaveri Nagunuri <kaveri.nagunuri@karmanya.co.in>
2016-06-07 10:40:55 +0530

    Eloquent Relations

--- database/DataTables/kaveri_Registration.sql
+++ database/DataTables/kaveri_Registration.sql
@@ -35,7 +35,7 @@ CREATE TABLE `Registration` (
   `Token` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`Id`),
   UNIQUE KEY `Email_UNIQUE` (`Email`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -44,7 +44,7 @@ CREATE TABLE `Registration` (
 
 LOCK TABLES `Registration` WRITE;
 /*!40000 ALTER TABLE `Registration` DISABLE KEYS */;
-INSERT INTO `Registration` VALUES (1,'Kaveri','Bapuji Nagar,Musheerabad,hyderabad','hyderabad','Telanagana','kaveri.nagunuri@karmanya.co.in','9908334108','8d88b5bdee8b5cb73df63e8dbd339797',NULL,NULL),(2,'Nagunuri Kaveri','D.No:1-79,Valasapakala,kakinada','kakinada','Andhra Pradesh','nagunurikaveri@gmail.com','7097810836','8ead3ac240793b5e7a783368918e1af2',NULL,NULL),(3,'Uday Kumar','Bapuji nagar,musheerabad,hyderabad-500020','hyderabad','Telanagana','nagunuriuday@gmail.com','9000928851','110a377ff9a9ae9426443b0cb0aa384f','eyJpdiI6Ind5XC95QlFxK2hTaFB1XC9EV1UzMHllQT09IiwidmFsdWUiOiJOTXlRN09jKzlRbU1QbmRVT3ZVeEFOd1BHem93RFJoWWRqNHZjYWEyOTBnPSIsIm1hYyI6IjdlNzFjNTAwMjJjOTMyODExMDhlMDhjOThlMTZmYzI1YzgxMDRmZjI4ZWE2ZDAwZWYyZmQwNmIzZjcxYjFhNjkifQ==',NULL);
+INSERT INTO `Registration` VALUES (1,'Kaveri','Bapuji Nagar,Musheerabad,hyderabad','hyderabad','Telanagana','kaveri.nagunuri@karmanya.co.in','9908334108','2e0c7bd11ca09f9ebb81e632968a387b','eyJpdiI6Imt5TUZYM0RoSkRFXC9rQ1Z1UUk3YTFBPT0iLCJ2YWx1ZSI6IjhvS3dQVk1rS2RHNktXdVRIQjd0eU10N0E5amVUcXdaN0RcL0xcL0tEbkFXST0iLCJtYWMiOiI2M2IyMWJhNmU4Njk5ZDRlZTY3ODViYTUxYTY1MGY1NzU0ZTMwZmExNGEzNWVkMDM2MjI4ZjRkM2JjMzMxMzcxIn0=','115224441828278551441'),(2,'Nagunuri Kaveri','D.No:1-79,Valasapakala,kakinada','kakinada','Andhra Pradesh','nagunurikaveri@gmail.com','7097810836','8ead3ac240793b5e7a783368918e1af2',NULL,NULL),(3,'Uday Kumar','Bapuji nagar,musheerabad,hyderabad-500020','hyderabad','Telanagana','nagunuriuday@gmail.com','9000928851','110a377ff9a9ae9426443b0cb0aa384f','eyJpdiI6Ind5XC95QlFxK2hTaFB1XC9EV1UzMHllQT09IiwidmFsdWUiOiJOTXlRN09jKzlRbU1QbmRVT3ZVeEFOd1BHem93RFJoWWRqNHZjYWEyOTBnPSIsIm1hYyI6IjdlNzFjNTAwMjJjOTMyODExMDhlMDhjOThlMTZmYzI1YzgxMDRmZjI4ZWE2ZDAwZWYyZmQwNmIzZjcxYjFhNjkifQ==',NULL),(4,'kaveri','D.no:1-6-174/29,Bapuji Nagar,Musheerabad,Hyderabad','hyderabad','Telanagana','kaveri2710@gmail.com','7097810836',NULL,'eyJpdiI6Ik85YjBpazdyMlVKdzV1V2o2YTdqeGc9PSIsInZhbHVlIjoieVwveTVRXC9sT0E2SW0xRVhlbmpiVk5YWnYwYklDQWxWSkdtVnJsd2RtUFZ3PSIsIm1hYyI6ImM5NGUyZDYxOWYwMmFmODJmZjRiZWVkYzUzNzc1NDVlYzBlMmRhYjgxZjJhMTRjY2FhZTI0YzI0OGVhMWE3ZTkifQ==','906473922814718');
 /*!40000 ALTER TABLE `Registration` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -57,4 +57,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-06-05 21:04:31
+-- Dump completed on 2016-06-07 10:39:14
